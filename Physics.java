393fe2d685e9c00b6478b213167f9783bba19ebc
unknown <s.juangphanic12@CUS-LIBLAB-3320.students.bchigh.edu>
2012-03-09 14:12:28 -0500

    Square One

--- Physics.java
+++ Physics.java
@@ -8,7 +8,7 @@ public class TrivialApplication
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         GUI gui = new GUI(d);
         
-        JFrame frame = new JFrame("Curves");
+        JFrame frame = new JFrame("Physics");
         frame.setSize(d);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
