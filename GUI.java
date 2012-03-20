dc2b28e8217779380597346e50f63af1fb6ccf74
unknown <s.juangphanic12@CUS-LINK-3404.students.bchigh.edu>
2012-03-20 12:47:40 -0400

    Side panel and basic animation engine

--- GUI.java
+++ GUI.java
@@ -3,14 +3,16 @@ import java.awt.*;
 import java.awt.image.*;
 import javax.swing.event.ChangeListener; 
 
-public class GUI extends JPanel
+public class GUI extends JPanel implements Runnable
 {
-    Dimension d;
-    JPanel inputPanel;
-    int width, height;
-    BufferedImage image;
-    JLabel imageL;
-    Graphics myG;
+    private Dimension d;
+    private JPanel inputPanel;
+    private int width, height;
+    
+    private Thread animator;
+    private final int DELAY = 2;
+    
+    private int x = 0, y = 0, speedX = 1, speedY = 1;
     
     public GUI(Dimension a) throws Exception
 	{
@@ -24,25 +26,72 @@ public class GUI extends JPanel
         width = this.getWidth();
         height = this.getHeight();
         
-        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        imageL = new JLabel(new ImageIcon(image));
-        imageL.setBounds(0,0,width,height);
-               
-        myG = image.getGraphics();
-        myG.setColor(Color.WHITE);
-
 		inputPanel = new JPanel();
 		inputPanel.setBackground(Color.DARK_GRAY);
 		inputPanel.setLayout(null);
 		inputPanel.setVisible(true);
-		inputPanel.setBounds((int)(width*0.7),0,(int)(width*0.3),height);
+		inputPanel.setBounds((int)(width*0.85),0,(int)(width*0.15),height);
 
 		this.add(inputPanel);
-        this.add(imageL);
     } 
+    
+    public void addNotify()
+    {
+    	super.addNotify();
+    	animator = new Thread(this);
+    	animator.start();
+    }
+    
+    public void cycle() 
+    {
+        x += speedX;
+        y += speedY;
+
+        if((x > (width*0.85))||(x < 0)) 
+        {
+            speedX *= -1;
+        }
+        if((y > height)||(y < 0)) 
+        {
+            speedY *= -1;
+        }
+    }
     
     public void paintComponent(Graphics g)
     {
-        super.paintComponent(g);         
-    }    
+        super.paintComponent(g);
+        
+        g.setColor(Color.WHITE);
+        g.fillOval(x,y,10,10);    
+    } 
+    
+    public void run() 
+    {
+        long beforeTime, timeDiff, sleep;
+
+        beforeTime = System.currentTimeMillis();
+
+        while (true) 
+        {
+            cycle();
+            repaint();
+
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+
+            if(sleep < 0)
+            {
+                sleep = 2;
+            }
+            
+            try 
+            {
+                Thread.sleep(sleep);
+            }catch(InterruptedException e) 
+            {
+            }
+
+            beforeTime = System.currentTimeMillis();
+        }
+    }   
 }
